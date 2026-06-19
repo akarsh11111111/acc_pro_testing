d8f14b82f4c2d6b2ebd3b59a1dcbbae8ec5cd367
akarshvidyarthi10-web <akarshvidyarthi10@gmail.com>
2026-06-19 13:16:45 +0530

    Fix syntax error in demo.js

--- demo.js
+++ demo.js
@@ -31,4 +31,5 @@ function loginSecure(db, userInput, passwordInput) {
   db.execute(query, [userInput, passwordInput]);
   console.log("Secure query executed with parameters.");
 }
-}}}}}}}}}}}}}}}}}}}}}}}}}}}
+}}}}}}}}}}}}}}}}}}}}}}}}}
+
