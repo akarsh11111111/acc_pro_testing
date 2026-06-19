9eba6a4612e13816a81b9d120c2b600b50825fce
akarsh <168165668+akarsh11111111@users.noreply.github.com>
2026-06-19 11:24:07 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello Akarsh 🚀</h1>
+      <p>React working perfectly ✅</p>
+      <button onClick={() => alert("Button clicked!")}>
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
